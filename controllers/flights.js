d0c1cf85a4ee7cfff7341f14aa2af126ac460bf0
Molly Calhoun <mollyfcalhoun@gmail.com>
2024-04-17 22:27:28 -0700

    Lab part 1 and beginning part 2 destination schema

---
+++ controllers/flights.js
@@ -0,0 +1,44 @@
+const Flight = require('../models/flight');
+
+module.exports = {
+    index,
+    show,
+    new: newFlight,
+    create,
+};
+
+
+async function index(req, res) {
+    const flights = await Flight.find({}).sort('departs');
+    res.render('flights/index', { flights });
+}
+
+async function show(req, res) {
+    const flight = await Flight.findById(req.params.id);
+}
+
+
+function newFlight(req, res) {
+    const newFlight = new Flight();
+    //Obtain the default date
+    const dt = newFlight.departs;
+    //Format the date for the value attribute 
+    //of the input
+    let departsDate = `${dt.getFullYear()}-${(dt.getMonth() + 1).toString().padStart(2, '0')}`;
+    departsDate += `-${dt.getDate().toString().padStart(2, '0')}T${dt.toTimeString().slice(0, 5)}`;
+    res.render('flights/new', { errorMsg: '', departsDate });
+}
+
+
+async function create(req, res) {
+    for (let key in req.body) {
+        if (req.body[key] === '') delete req.body[key];
+    }
+    try {
+        await Flight.create(req.body);
+        res.redirect('/flights');
+    } catch (err) {
+        console.log(err);
+        res.render('flights/new', { errorMsg: err.message });
+    }
+}
---
+++ routes/flights.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const flightsCtrl = require('../controllers/flights');
+
+// default /flights
+// GET /flights (index)
+router.get('/', flightsCtrl.index);
+// GET /new (new action)
+router.get('/new', flightsCtrl.new);
+// GET /flights/:id (show action) MUST be below new route
+router.get('/:id', flightsCtrl.show);
+// POST /flights (post action)
+router.post('/', flightsCtrl.create);
+
+module.exports = router;
